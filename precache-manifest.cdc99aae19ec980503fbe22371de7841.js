0ac07cf5e81f22c5edb1839a3f7e095e44abdbc3
natalie246 <natalieakiva@gmail.com>
2019-06-15 12:51:40 +0300

    Updates

---
+++ precache-manifest.cdc99aae19ec980503fbe22371de7841.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "2873375fc50fb304d268",
+    "url": "/face-recognition/static/js/runtime~main.4924a73e.js"
+  },
+  {
+    "revision": "41d2ed67de6d39e8d203",
+    "url": "/face-recognition/static/js/main.85aad616.chunk.js"
+  },
+  {
+    "revision": "83f558eb0738be550951",
+    "url": "/face-recognition/static/js/2.7ca30bdf.chunk.js"
+  },
+  {
+    "revision": "41d2ed67de6d39e8d203",
+    "url": "/face-recognition/static/css/main.8c30658e.chunk.css"
+  },
+  {
+    "revision": "83f558eb0738be550951",
+    "url": "/face-recognition/static/css/2.9351b849.chunk.css"
+  },
+  {
+    "revision": "c4d8f6c6e3fd046db9f9e1be8b4839ed",
+    "url": "/face-recognition/index.html"
+  }
+];
